51c463148ebe519968537c8c11f8b6eb9fea5909
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-26 21:36:17 -0500

    organized functions

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import { openModal, closeModal } from "./add-project-modal"
 import { Project, Task } from "./projects"
 
-function changeView(view) {
-    const content = document.querySelector('#content')
-    content.innerHTML = view
-}
+const navigation = (function() {
+    function changeView(view) {
+        const content = document.querySelector('#content')
+        content.innerHTML = view
+    }
+    
+    function getContent(e) {
+        e.preventDefault()
+        changeView(e.target.innerHTML)
+        hilightElement(e)
+    }
+    
+    function hilightElement(e) {
+        const inbox = document.querySelectorAll('.inbox-category')
+        inbox.forEach(function(element) {
+            element.style.backgroundColor = 'inherit';
+        })
+        const parent = e.target.parentNode;
+        parent.style.backgroundColor = 'var(--hilight-sidebar)';
+    }
 
-function setNavEvents() {
-    const inbox = document.querySelectorAll('.inbox-category')
-    inbox.forEach(function(menu) {
-        const link = menu.querySelector('a')
-        link.addEventListener('click', getContent)
-    })
-}
+    function setup() {
+        const inbox = document.querySelectorAll('.inbox-category')
+        inbox.forEach(function(menu) {
+            const link = menu.querySelector('a')
+            link.addEventListener('click', getContent)
+        })
+    }
 
-function getContent(e) {
-    e.preventDefault()
-    changeView(e.target.innerHTML)
-    hilightElement(e)
-}
-
-function hilightElement(e) {
-    const inbox = document.querySelectorAll('.inbox-category')
-    inbox.forEach(function(element) {
-        element.style.backgroundColor = 'inherit';
-    })
-    const parent = e.target.parentNode;
-    parent.style.backgroundColor = 'var(--hilight-sidebar)';
-}
+    return {setup}
+})();
 
 const projectsTab = (function() {
     function addProject(e) {
@@ -54,6 +58,6 @@ const projectsTab = (function() {
 })();
 
 const initialPageLoad = (function() {
-    setNavEvents()
+    navigation.setup()
     projectsTab.setup()
 })();
